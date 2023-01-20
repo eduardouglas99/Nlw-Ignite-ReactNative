85a5e92a000099b1956b9abfd5f5df44b08b0b0e
Eduardo Douglas de Melo Caetano <ecaetano@marlin.net>
2023-01-20 17:25:35 -0300

    criacao da home junto aos componentes header e habit day

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import './src/lib/dayjs';
+import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold} from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   //Forma de garantir o carregamento da font usada no projeto
@@ -16,23 +17,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+    <>
+      <Home />
       <StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-    fontSize: 32,
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
